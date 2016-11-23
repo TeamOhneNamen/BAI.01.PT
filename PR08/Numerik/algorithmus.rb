f4a7b3ace60d29271a7c4f6a3054647335166e90
Gargamel19 <trendelenburger19.04@googlemail.com>
2016-11-23 16:58:34 +0100

    jhrgh

--- PR08/Numerik/algorithmus.rb
+++ PR08/Numerik/algorithmus.rb
@@ -1,25 +1,27 @@
 class Algorithm
-  
   def solve(x ,n, i = 1, t = 1)
-    if ((x < 0) || !x.is_a?(Integer) || (x > 2))
+    if (x.is_a?(Integer))
+      x = x.to_f
     end
-    @solution = 0.0
-    t.times{
-    n.to_i.times { # sum of the term: (((-1)**(i+1)) * (((x - 1) ** i) / i))
-      @solution = @solution + (((-1)**(i+1)) * (((x - 1) ** i) / i))
-      i += 1
-    }
-    
-    n += 1
-    }
-    n -= 1
-    puts @solution
-    puts n
-    puts "---" #borderline between calculation and aim
-    puts Math.log(x) # natural logarithm
+    if ((x < 0.0) || (x > 2.0))
+        raise ArgumentError
+    else
+      @solution = 0.0
+      n.to_i.times { # sum of the term: (((-1)**(i+1)) * (((x - 1) ** i) / i))
+        @solution = @solution + (((-1)**(i+1)) * (((x - 1) ** i) / i))
+        i += 1
+      }
+      n -= 1
+      puts @solution
+      puts n
+      puts "---" #borderline between calculation and aim
+      puts Math.log(x) # natural logarithm
+
+    end
   end
-  
+
 end
+
 alg = Algorithm.new()
 #alg.solve(2, 1.0, 1.0, 10000) # way I calculated towards a relative precise
 #-> from n = 10000 is the calculation precise on six positions after float point
